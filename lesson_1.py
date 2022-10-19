ff8f68570b6f03447ba1dc01573e86a5419c4d5d
unknown <savkom513@gmail.com git config --global user.name>
2022-10-19 18:31:03 +0300

    19.10

---
+++ lesson_1.py
@@ -0,0 +1,15 @@
+
+
+
+def hi(kak, ti):
+    if not isinstance(kak, int) or not isinstance(ti, int):
+        raise Exception('я принимаю только числа')
+    elif sum == 5:
+        sum = kak + ti
+        print(sum / 2)
+    else:
+        return (kak + ti)
+hi(3, 2)
+
+
+
