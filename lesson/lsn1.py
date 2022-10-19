0349a7adb19487cbe01ef3d0d41f2d5a13225b83
PashkaSharl <82102965+PashkaSharl@users.noreply.github.com>
2022-10-19 18:16:19 +0300

    19.10

---
+++ lesson/lsn1.py
@@ -0,0 +1,13 @@
+def fn(value):
+    if not isinstance(value, int):
+        raise Exception('Я принимаю только числа')
+    elif value > 34:
+        print('mama')
+    else:
+        print('my_name')
+
+
+
+
+
+
