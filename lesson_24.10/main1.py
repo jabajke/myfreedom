2218928a27a92fc9204be5aceb98bf5094260102
strelkosasha <sashastrelko@mail.ru>
2022-10-24 18:25:00 +0300

    lesson_24.10

--- lesson_24.10/main1.py
+++ lesson_24.10/main1.py
@@ -1,1 +1,14 @@
-print('ahaha')
+def neogran(*args):
+    print(sum(args))
+neogran(1, 2, 3, 4, 5)
+
+
+def mnogo(**kawargs):
+    if 'privet' in kawargs.values():
+        kawargs['privet'] = 'poka'
+        print(kawargs)
+    else:
+        raise Exception('нету')
+mnogo(asd='ff', qwe='privet', privet= 'adad')
+
+
