a67060dd0acc6de037a6c1e44353c72d24a3c5e5
qwyxtt <you@example.com>
2022-10-30 03:03:01 +0300

    hi

--- lesson_1.py
+++ lesson_1.py
@@ -4,12 +4,11 @@
 def hi(kak, ti):
     if not isinstance(kak, int) or not isinstance(ti, int):
         raise Exception('я принимаю только числа')
-    elif sum == 5:
-        sum = kak + ti
-        print(sum / 2)
+    elif kak + ti == 5:
+        print(5 / 2)
     else:
         return (kak + ti)
-hi(3, 2)
+print(hi('sasha chmo', 2))
 
 
 
